251c22ec01d448b08a675d8429106bd247c96ae9
Krishna Keerthan <krishnakeerthan828@gmail.com>
2025-08-13 15:54:58 +0530

    second deployment

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
